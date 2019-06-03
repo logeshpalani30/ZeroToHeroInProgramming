53f214b873fff12c642013dcd6992c423b9a4c4e
LOGESH PALANI <logesh.01@hotmail.com>
2019-06-03 10:05:05 +0530

    FirstCommit

---
+++ FullNameToSplitName.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+
+namespace PlayingwithCSharp
+{
+    public class FullNametoSplitName
+    {
+        public static void FullNameto()
+        {
+            string str = "Logesh Palani";
+            ArrayList arrayList = new ArrayList();
+            string Temp = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] != ' ')
+                {
+                    Temp = Temp + str[i];
+                    if(i + 1 == str.Length)
+                    {
+                        arrayList.Add(Temp);
+                        Temp = String.Empty;
+                    }
+
+                }
+                else
+                {
+                    arrayList.Add(Temp);
+                    Temp = String.Empty;
+                }
+                
+
+            }
+            //var inte = arrayList.Count;
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                System.Console.WriteLine(arrayList[i]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace PlayingwithCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberToWord = NumberToWord.ConvertNumbertoWords(3056);
+            Console.WriteLine(numberToWord);
+            FullNametoSplitSeparateNames();
+            Console.ReadLine();
+        }
+
+         public static void FullNametoSplitSeparateNames()
+        {
+            string str = "Logesh Palani";
+            ArrayList arrayList = new ArrayList();
+            string Temp = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] != ' ')
+                {
+                    Temp = Temp + str[i];
+                    if(i + 1 == str.Length)
+                    {
+                        arrayList.Add(Temp);
+                        Temp = String.Empty;
+                    }
+
+                }
+                else
+                {
+                    arrayList.Add(Temp);
+                    Temp = String.Empty;
+                }
+                
+
+            }
+            //var inte = arrayList.Count;
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                System.Console.WriteLine(arrayList[i]);
+            }
+            
+          //  Console.ReadLine();
+        }
+    }
+}
