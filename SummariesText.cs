fe9faeb5a56884f2d74eb3439df0b6831daab6d4
Logesh Palani <logeshmeena98@gmail.com>
2019-07-28 12:26:30 +0000

    SummarText

---
+++ SummariesText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn
+{
+    public class SummariesText
+    {
+        public static void SummariesTextDisplay(string text)
+        {
+            var maxLength = 20;
+            if (text.Length < maxLength)
+                Console.WriteLine(text);
+            else
+            {
+                var wordsList = new List<string>();
+                var words = text.Split(' ');
+                var wordLength = 0;
+                foreach (var item in words)
+                {
+                    wordsList.Add(item);
+                    wordLength += item.Length + 1;
+                    if (wordLength > maxLength)
+                        break;
+                }
+
+                var summary = String.Join(' ', wordsList) + "...";
+                Console.WriteLine(summary);
+            }
+        }
+    }
+}
