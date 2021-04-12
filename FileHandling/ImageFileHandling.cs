05ca5d90a225914fe0dac2ee91294d50e830a86c
Logesh Palani <logesh.01@hotmail.com>
2021-04-13 02:59:57 +0530

    added pick file sample

---
+++ FileHandling/ImageFileHandling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PlayingwithCSharp.FileHandling
+{
+    public class ImageFileHandling
+    {
+        public byte[] ReadFileFromLocal(string exePath)
+        {
+            try
+            {
+                byte[] buf = new byte[1024];
+                int c;
+
+                using (FileStream fs = File.OpenRead(exePath))
+                {
+                    while ((c = fs.Read(buf, 0, buf.Length)) > 0)
+                    {
+                        Console.WriteLine(Encoding.UTF8.GetString(buf, 0, c));
+                    }
+                }
+
+                return buf;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using PlayingwithCSharp.FileHandling;
 
 namespace PlayingwithCSharp
 {
@@ -9,7 +10,14 @@ namespace PlayingwithCSharp
     {
         static void Main(string[] args)
         {
-           
+
+            string exePath =@"C:\ZeroToHeroInProgramming\image.jfif";
+                // System.IO.Path.GetDirectoryName(
+                //     System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            ImageFileHandling imageFileHandling = new ImageFileHandling();
+            imageFileHandling.ReadFileFromLocal(exePath);
+
         }
     }
 }
