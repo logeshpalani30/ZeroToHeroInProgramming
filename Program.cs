0ac78f8f50de8fff783f677a9f50838c2ed8b35f
Logesh Palani <logesh.01@hotmail.com>
2021-04-21 00:12:13 +0530

    learned reverse int program

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ namespace PlayingwithCSharp
         {
             var number = Convert.ToInt32(23456);
             System.Console.WriteLine(number);
-            var result = (number%10)/1*10000;
-            result = result+ ((number % 100) / 10) * 1000;
-            result = result+ ((number % 1000) / 100) * 100;
-            result = result +(((number%10000)/1000)*10);
-            result = result +(((number%100000)/10000)*1);
+            var revered_int= 0;
 
-            System.Console.WriteLine(result);
+            while (true)
+            {
+                revered_int= (revered_int*10)+number%10;
+                number = number/10;
+                
+                if (number==0)break;
+            }
+            System.Console.WriteLine(revered_int);
         }
     }
     public class ReactableAndCircle{
